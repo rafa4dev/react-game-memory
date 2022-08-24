162abd18d9da069658fda38fec470f59d456d06e
Rafael Paz <dudex1992@gmail.com>
2022-08-24 16:18:54 -0300

    adding files to repo

---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from 'react';
+import * as C from './App.styles';
+
+import logoImage from './assets/devmemory_logo.png';
+import restartIcon from './svgs/restart.svg';
+
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
+import { GridItem } from './components/GridItem';
+
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
+
+const App = () => {
+
+  const [playing, setPlaying] = useState<Boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  useEffect(()=> {
+    const timer = setInterval(()=>{
+      if(playing) setTimeElapsed(timeElapsed + 1);
+    },1000);
+    return () => clearInterval(timer);
+  },[playing, timeElapsed]);
+
+  //verify if opened are equal
+  useEffect(()=>{
+    if(shownCount === 2){
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2){
+
+        // verificação 1 - if both are equal, make every "shown" permanent
+        if(opened[0].item === opened[1].item){
+          let tmpGrid = [...gridItems];
+          for(let i in tmpGrid){
+            if(tmpGrid[i].shown){
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid)
+          setShownCount(0);
+        } else {
+          setTimeout(()=> {
+            let tmpGrid = [...gridItems];
+          for(let i in tmpGrid){
+            tmpGrid[i].shown = false;
+          }
+          setGridItems(tmpGrid)
+          setShownCount(0);
+          }, 1000)
+        }
+        
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  },[shownCount, gridItems]);
+
+  useEffect(()=> {
+    if(moveCount > 0  && gridItems.every(item => item.permanentShown === true)) {
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems])
+
+  const resetAndCreateGrid = () => {
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
+
+    let tmpGrid: GridItemType[] = [];
+
+    // Criar um grid vazio
+    for(let i = 0; i < (items.length * 2); i++) tmpGrid.push({ item: null, shown: false, permanentShown: false });
+
+    // Prenche o Grid
+    for(let w=0; w < 2; w++) {
+      for(let i = 0; i < items.length; i++) {
+        // Gera uma posicao até 12
+        let pos = -1;
+        while(pos < 0 || tmpGrid[pos].item !== null){
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tmpGrid[pos].item = i;
+      }
+    }
+
+    // Jogar no state
+    setGridItems(tmpGrid);
+
+    setPlaying(true);
+  }
+
+  const handleItemClick = (index: number) => {
+    if(playing && index !== null && shownCount < 2){
+      let tmpGrid = [...gridItems];
+      if(!tmpGrid[index].permanentShown && !tmpGrid[index].shown){
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
+  }
+
+  return(
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+        <C.InfoArea>
+          <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
+          <InfoItem label="Movimentos" value={moveCount.toString()} />
+        </C.InfoArea>
+        <Button label="Reiniciar" icon={restartIcon} onClick={resetAndCreateGrid}/>
+      </C.Info>
+      <C.GridArea>
+        <C.Grid>
+          {gridItems.map((item, index)=>(
+            <GridItem 
+              key={index}
+              item={item}
+              onClick={() => handleItemClick(index)}
+            />
+          ))}
+        </C.Grid>
+      </C.GridArea>
+    </C.Container>
+  )
+}
+
+export default App;
